44d4096b66cc67d2ab368cd355b0d948b0857608
Jonathan Dorety <kyullog@gmail.com>
2020-02-22 11:05:26 -0800

    add some dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Alert from "react-bootstrap/Alert";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Alert variant="success" dismissible>
+        <Alert.Heading>This is a test alert</Alert.Heading>
+        <p>Yeah, just seeing how this looks.</p>
+      </Alert>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
