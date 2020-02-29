e2288b1d0ea1ec7a787a8817e22b51dd171ef21b
Jonathan Dorety <jonathan.dorety@gmail.com>
2020-02-29 10:39:57 -0800

    PartyView fetches correct party data

--- src/components/PartyView/PartyView.js
+++ src/components/PartyView/PartyView.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const PartyView = props => (
-    <h1>This is a party</h1>
-)
+import axios from "../../util/axios";
+
+const PartyView = props => {
+  const axiosCall = axios.axiosHeaders();
+
+  const [party, setParty] = useState({});
+
+  useEffect(() => {
+    const partyId = props.match.params.id;
+    axiosCall
+      .get(`/api/party/${partyId}`)
+      .then(res => setParty(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return <h1>This is a party</h1>;
+};
 
 export default PartyView;
