707b85e2ab3d7f47aa67c67ef8674c8b89d379dc
Jonathan Dorety <jonathan.dorety@gmail.com>
2020-02-29 12:15:39 -0800

    Clean up imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 import PartyList from "./components/PartyList";
 import PartyView from "./components/PartyView/PartyView"
