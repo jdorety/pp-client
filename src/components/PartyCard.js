ee013ff9fbd508728e30308428d31d4f794804d4
Jonathan Dorety <jonathan.dorety@gmail.com>
2020-02-29 09:49:36 -0800

    Party link list populates

--- src/components/PartyCard.js
+++ src/components/PartyCard.js
@@ -1,30 +1,24 @@
 import React from "react";
-import { Accordion, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import People from "../Icons/People";
 import Calendar from "../Icons/Calendar";
 
 const PartyCard = props => (
-  <Accordion>
-    <Card>
-      <Accordion.Toggle as={Card.Header} eventKey={props.id}>
-        <Card.Title>{props.theme}</Card.Title>
-      </Accordion.Toggle>
-      <Accordion.Collapse eventKey={props.id}>
-        <Card.Body>
-          <Card.Text>
-            <div>
-              <Calendar height="40" width="40" />
-              <span>{props.when}</span>
-            </div>
-            <div>
-              <People height="40" width="40" />
-              <span>{props.numberGuest}</span>
-            </div>
-          </Card.Text>
-        </Card.Body>
-      </Accordion.Collapse>
-    </Card>
-  </Accordion>
+  <Card>
+    <Card.Body>
+      <Card.Title>{props.theme}</Card.Title>
+      <Card.Text>
+        <div>
+          <Calendar height="40" width="40" />
+          <span>{props.when}</span>
+        </div>
+        <div>
+          <People height="40" width="40" />
+          <span>{props.numberGuest}</span>
+        </div>
+      </Card.Text>
+    </Card.Body>
+  </Card>
 );
 
 export default PartyCard;
