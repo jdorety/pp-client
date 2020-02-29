8b22b61c108cb9406a3611377f7d681163678916
Jonathan Dorety <jonathan.dorety@gmail.com>
2020-02-29 12:16:22 -0800

    Debug axios calls

--- src/components/PartyList.js
+++ src/components/PartyList.js
@@ -3,11 +3,9 @@ import axios from "../util/axios";
 import { Container } from "react-bootstrap";
 import PartyCard from "./PartyCard";
 
+const axiosCall = axios.axiosHeaders();
 const PartyList = props => {
   const [parties, setParties] = useState([]);
-
-  const axiosCall = axios.axiosHeaders();
-
 
   useEffect(() => {
     axiosCall
@@ -19,11 +17,7 @@ const PartyList = props => {
   return (
     <Container>
       {parties.map(party => (
-        <PartyCard
-          {...party}
-          key={party.id}
-          history={props.history}
-        />
+        <PartyCard {...party} key={party.id} history={props.history} />
       ))}
     </Container>
   );
--- src/components/PartyView/PartyView.js
+++ src/components/PartyView/PartyView.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from "react";
 
 import axios from "../../util/axios";
+const axiosCall = axios.axiosHeaders();
 
 const PartyView = props => {
-  const axiosCall = axios.axiosHeaders();
-
   const [party, setParty] = useState({});
 
   useEffect(() => {
     const partyId = props.match.params.id;
     axiosCall
       .get(`/api/party/${partyId}`)
-      .then(res => setParty(res.data))
+      .then(res => {
+        console.log("ping");
+        setParty(res.data);
+      })
       .catch(err => console.log(err));
-  }, []);
+  }, [props.match.params.id]);
 
-  return <h1>This is a party</h1>;
+  return <h3>{party.theme}</h3>;
 };
 
 export default PartyView;
