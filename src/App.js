0194ee8d6fb743fb6352d881060a6f881e906ab2
Jonathan Dorety <jonathan.dorety@gmail.com>
2020-02-29 10:31:26 -0800

    Create dummy PartyView component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import PartyList from "./components/PartyList";
+import PartyView from "./components/PartyView/PartyView"
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Route path="/" exact component={PartyList} />
+      <Route path="/parties/:id" component={PartyView} />
     </div>
   );
 }
---
+++ src/components/PartyView/PartyView.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PartyView = props => (
+    <h1>This is a party</h1>
+)
+
+export default PartyView;
