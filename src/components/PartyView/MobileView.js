8ddce68dac6e992d99f3ace12f98076bd1fdb3d7
Jonathan Dorety <jonathan.dorety@gmail.com>
2020-03-01 10:19:28 -0800

    Begin laying out mobile party view

---
+++ src/components/PartyView/MobileView.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+import InfoCard from "./InfoCard";
+/**
+ * Mobile view
+ * @prop {string} theme The theme, or name of the party
+ * @prop {string} username The username of the party host
+ * @prop {string} when The date (MM/DD/YYYY) the party is to be held on
+ * @prop {number} numberGuest The guest count for the party
+ * @prop {number} budget The monetary budget set for the party
+ * @prop {number} spentBudget The amount of money spent on the party so far
+ */
+const MobileView = props => {
+  return (
+    <>
+      <Jumbotron fluid>
+        <h3>{props.theme}</h3>
+      </Jumbotron>
+      <InfoCard {...props} />
+    </>
+  );
+};
+
+export default MobileView;
