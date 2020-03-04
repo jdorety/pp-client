62f684e3bc143588a225fe3ff2139804e239355a
Jonathan Dorety <kyullog@gmail.com>
2020-03-03 20:17:20 -0800

    Debug reducer

--- src/reducers/TodoReducer.js
+++ src/reducers/TodoReducer.js
@@ -18,9 +18,9 @@ export default function TodoReducer(state, action) {
     case TOGGLE_START:
       const copyList = [...state.todos];
       copyList[action.index] = action.item;
-      return { ...state, todos: copyList, updating: true };
+      return { ...state, todos: copyList, updating: true, error: "" };
     case TOGGLE_SUCCESS:
-      return { ...state, updating: false };
+      return { ...state, updating: false, error: "" };
     case TOGGLE_FAILURE:
       return { ...state, updating: false, error: action.error };
     default:
