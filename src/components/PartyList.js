7c4f0d7db5fce43c909cacd2b6f84efe5be5eba1
Jonathan Dorety <jonathan.dorety@gmail.com>
2020-03-09 10:26:08 -0700

    Create function to convert Date for display

--- src/components/PartyList.js
+++ src/components/PartyList.js
@@ -9,18 +9,31 @@ const PartyList = props => {
   const [parties, setParties] = useState([]);
   const userId = 2;
 
+  const convertDate = party => {
+    const date = new Date(party.when);
+    console.log(date);
+    const displayDate = date.toDateString();
+    console.log(displayDate);
+    return { ...party, when: displayDate };
+  };
+
   useEffect(() => {
     axiosCall
       .get(`/api/user/${userId}/parties`)
-      .then(res => setParties(res.data))
+      .then(res => {
+        const formatData = res.data.map(party => {
+          return convertDate(party);
+        });
+        setParties(formatData);
+      })
       .catch(err => console.log(err));
   }, []);
 
   return (
     <Container>
-        {parties.map(party => (
-          <PartyCard {...party} key={party.id} history={props.history} />
-        ))}
+      {parties.map(party => (
+        <PartyCard {...party} key={party.id} history={props.history} />
+      ))}
     </Container>
   );
 };
